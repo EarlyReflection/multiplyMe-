9396edcd62749254ecc2c23456d3abda6587a097
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-08-26 19:00:52 +0300

    Initial Commit

---
+++ multiplyMe!/multiplyMe_App.swift
@@ -0,0 +1,17 @@
+//
+//  multiplyMe_App.swift
+//  multiplyMe!
+//
+//  Created by Vladimir Dvornikov on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct multiplyMe_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
